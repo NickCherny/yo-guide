89fc798e93399f2112ba6ae22df365a84e4279d8
nick <cherny.web@gmail.com>
2016-08-22 17:44:36 +0300

    delete client react

--- public/js/developer/react/components/forms.components/login.component.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-
-let LoginUserApp = React.createClass({
-  render: function(){
-    
-  }
-});
--- public/js/developer/react/store/configStore.js
+++
@@ -1,7 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers/index';
-
-export default function configStore(initialState){
-  const store = createStore(rootReducer, initialState);
-  return store;
-}
